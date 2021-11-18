f3e04594c088785cccfa9fc8e9be4a3ea3c0b96f
Albaer33 <alisi3396@gmail.com>
2021-11-18 16:58:31 +0100

    container popolato

--- milestone2/js/script.js
+++ milestone2/js/script.js
@@ -12,7 +12,7 @@ const title = [
     'Gran Bretagna',
     'Germania',
     'Paradise'
-]
+];
 
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
@@ -20,4 +20,27 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+];
+
+// importo gli oggetti da html
+const imgContainer=document.querySelector('.images-container');
+
+for(let i=0; i<items.length; i++) {
+    const thisImg=items[i];
+    const thisTitle=title[i];
+    const thisText=text[i];
+
+    // popolo l'img container
+    const newImg=`
+        <div class="single-image">
+            <img src="${thisImg}" alt="Img ${i+1}">
+            <div class="text">
+                <h3>${thisTitle}</h3>
+                <p>${thisText}</p>
+            </div>
+        </div>
+    `;
+    imgContainer.innerHTML += newImg;
+}
+const textHeadContainer=document.querySelector('.text h3');
+const textParagraphContainer=document.querySelector('.text p');
